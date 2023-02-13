85434888439171e1759f0493a3c13e66ef998944
david-commit <davidondiege@gmail.com>
2023-02-13 12:35:36 +0300

    Initialized Vite + React + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <div className='App'>
+      <h1 class='text-4xl font-bold underline'>Hello world!</h1>
+<button class="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">...</button>
+
+<button class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">...</button>
+    </div>
+  );
+}
+
+export default App
